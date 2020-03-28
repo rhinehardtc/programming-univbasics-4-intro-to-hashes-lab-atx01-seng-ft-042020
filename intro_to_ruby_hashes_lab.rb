c182f35961cbd18e7fffe3a0f4c01270c4c3305e
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-03-28 03:27:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,9 +38,14 @@ end
 
 def update_counting_hash(hash, key)
   
-  hash[key] ? hash[key] += 1 : hash[key] = 1
+  
   
   if hash[key]
     hash[key] += 1
+    return hash
+  else
+    hash[key] = 1
+    return hash
+  end
   
 end
