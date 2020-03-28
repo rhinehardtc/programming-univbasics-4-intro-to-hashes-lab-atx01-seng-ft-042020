019bfdbc6bb67a367ca924ebbbc8062ba9394925
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-03-28 02:25:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,6 @@ def my_hash
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   pioneer = {
     :name => 'Grace Hopper'
   }
@@ -17,6 +16,7 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  :id => 2
 end
 
 def my_hash_creator(key, value)
